d3d1018e0ce57b9ca54d294030731bbfc77b3f9a
Rach17 <rachediabderrahmane17@gmail.com>
2026-07-20 14:59:56 +0100

    add unit tests

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,85 @@
+package auth
+
+import (
+    "testing"
+	"net/http"
+	"errors"
+	"cmp"
+)
+
+// func TestSplit(t *testing.T) {
+//     tests := map[string]struct {
+//         input string
+//         sep   string
+//         want  []string
+//     }{
+//         "simple":       {input: "a/b/c", sep: "/", want: []string{"a", "b", "c"}},
+//         "wrong sep":    {input: "a/b/c", sep: ",", want: []string{"a/b/c"}},
+//         "no sep":       {input: "abc", sep: "/", want: []string{"abc"}},
+//         "trailing sep": {input: "a/b/c/", sep: "/", want: []string{"a", "b", "c"}},
+//     }
+
+//     for name, tc := range tests {
+//         t.Run(name, func(t *testing.T) {
+//             got := Split(tc.input, tc.sep)
+//             diff := cmp.Diff(tc.want, got)
+//             if diff != "" {
+//                 t.Fatalf(diff)
+//             }
+//         })
+//     }
+// }
+
+func TestGetAPIKey(t *testing.T) { 
+	tests := map[string]struct {
+		input http.Header
+		want struct {
+			apiKey string
+			err    error
+		}
+	}{
+		"no auth header": {
+			input: http.Header{},
+			want: struct {
+				apiKey string
+				err    error
+			}{
+				apiKey: "",
+				err:    ErrNoAuthHeaderIncluded,
+			},
+		},
+		"invalid auth header": {
+			input: http.Header{
+				"Authorization": []string{"Bearer"},
+			},
+			want: struct {
+				apiKey string
+				err    error
+			}{
+				apiKey: "",
+				err:    errors.New("malformed authorization header"),
+			},
+		},
+		"valid auth header": {
+			input: http.Header{
+				"Authorization": []string{"ApiKey 123"},
+			},
+			want: struct {
+				apiKey string
+				err    error
+			}{
+				apiKey: "123",
+				err:    nil,
+			},
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			gotAPIKey, gotErr := GetAPIKey(tc.input)
+			if cmp.Compare(gotAPIKey, tc.want.apiKey) != 0 || cmp.Compare(gotErr.Error(), tc.want.err.Error()) != 0 {
+				t.Fatalf("GetAPIKey(%v) = (%v, %v), want (%v, %v)", tc.input, gotAPIKey, gotErr, tc.want.apiKey, tc.want.err)
+			}
+		})
+	}	
+}
